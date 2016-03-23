5a69b04c5525c16edc610901ef1fa0d679c0e810
Scott Mueller <sjmule@comcast.net>
2016-03-22 21:55:05 -0500

    added content

---
+++ js/links.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$("#openWings").click(function() {
+		window.location.href = "./openWings/index.html"
+	});
+	$("#wingColors").click(function() {
+		window.location.href = "wingColors/"
+	});
+});
